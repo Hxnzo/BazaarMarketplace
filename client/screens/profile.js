8cb7145e8fdcd6583afca36fedd884c181487971
Hanzalah Patel <hanzalahp20@gmail.com>
2024-10-27 19:49:22 -0400

    fixed styling

--- client/screens/profile.js
+++ client/screens/profile.js
@@ -178,6 +178,7 @@ const styles = StyleSheet.create({
 
   container: { 
     flex: 1,
+    justifyContent: 'center',
     paddingHorizontal: 20,
     backgroundColor: '#222831',
     alignItems: 'center',
