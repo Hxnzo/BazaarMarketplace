18efaabe03adcf0b0cfc2fc96620ffaca75da79c
Hanzalah Patel <hanzalahp20@gmail.com>
2024-11-27 22:10:08 -0500

    created .env file

--- client/firebase.js
+++ client/firebase.js
@@ -4,12 +4,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDBRGMce36b39aJhOPQcZKd1qc85crPhkY",
-  authDomain: "finalproject-fdf98.firebaseapp.com",
-  projectId: "finalproject-fdf98",
-  storageBucket: "finalproject-fdf98.firebasestorage.app",
-  messagingSenderId: "211641199237",
-  appId: "1:211641199237:web:47ddcc5e557058fcfb57ec"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase app
